fcb3b8c4c74223db7a3756bb2d221912a48e0dc0
TianPuJun <3128379695@qq.com>
2018-08-05 15:48:28 +0800

    fff

--- src/main/java/com/boot/demo/DemoApplication.java
+++ src/main/java/com/boot/demo/DemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 //@MapperScan({"com.boot.demo.dao"})
 public class DemoApplication {
+    //测试一下
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
